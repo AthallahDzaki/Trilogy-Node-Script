daa9707f02b8653f53979e00efb139fb4754882f
Athallah Dzaki <athallahdzaki@gmail.com>
2024-09-10 23:21:31 +0700

    Migrate

--- migrate.bat
+++ migrate.bat
@@ -5,11 +5,8 @@ COPY gifts.json gifts.json.bak
 
 where git > nul 2>&1 && (
     IF EXIST .git (
-        call git add .
-        call git commit -m "Migration"
-        call git stash
-        call git pull origin master --allow-unrelated-histories 
-        call git stash clear
+        call git fetch
+        call git reset --hard origin/master
     ) ELSE (
         echo "Git not found, you can manually download the latest version from https://github.com/AthallahDzaki/Trilogy-Node-Script/releases"
     )    
