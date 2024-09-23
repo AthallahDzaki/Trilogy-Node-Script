cf18dd2c52ff57d738c597086ecf43d4f226a8aa
Athallah Dzaki <athallahdzaki@gmail.com>
2024-09-23 15:01:07 +0700

    feat: Add Sorting to update-gift.js & Image update

--- etc/update-gift.js
+++ etc/update-gift.js
@@ -12,6 +12,7 @@ import { readFileSync, writeFileSync } from "fs";
     }
     */
     let ourGift = JSON.parse(readFileSync("./gifts.json", "utf8"));
+    let ourGiftImage = JSON.parse(readFileSync("./gift-img.json", "utf8"));
     let giftData = gift.data;
     giftData.forEach((element) => {
         let contex = {
@@ -26,6 +27,27 @@ import { readFileSync, writeFileSync } from "fs";
             ourGift.push(contex);
         }
 
-        writeFileSync("./gifts.json", JSON.stringify(ourGift, null, 4), "utf8");
+        /*
+        {
+            "id": 6064,
+            "name": "GG",
+            "image": "https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/3f02fa9594bd1495ff4e8aa5ae265eef~tplv-obj.webp"
+        },
+        */
+
+        if (ourGiftImage.find((x) => x.id == element.id) == undefined) {
+            console.log("New Gift Image: " + element.name);
+            // giftArr.push({"id" : gift.id, "name" : gift.name, "image" : gift.image.url_list[0]})
+            ourGiftImage.push({
+                id: element.id,
+                name: element.name,
+                image: element.image.url_list[0]
+            });
+        }
     })
+
+    ourGift.sort((a, b) => a.diamond_count - b.diamond_count); // Sort by diamond_count
+
+    writeFileSync("./gifts.json", JSON.stringify(ourGift, null, 4), "utf8");
+    writeFileSync("./gift-img.json", JSON.stringify(ourGiftImage, null, 4), "utf8");
 })();
