35975042231fa37409708286b8be05067bf7a893
Athallah Dzaki <athallahdzaki@gmail.com>
2024-12-21 00:42:42 +0700

    Version Only

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ import { API } from "./function/api.js";
 
 let wsServer = new WebSocketServer({ port: GeneralConfig.General.GUIWebsocketPort || 42069 });
 
-let g_sVersion = "SA CHAOS V1.6.1a";
+let g_sVersion = "SA CHAOS V1.6.1b";
 let g_Version = -1;
 let g_VersionString = "";
 
